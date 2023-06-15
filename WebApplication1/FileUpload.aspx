899da8ae71b6c35ebeb9dd71341929c5c7561af3
Lingesh <11985@na.proskauer.com>
2023-06-15 06:45:14 -0700

    version 1

---
+++ WebApplication1/FileUpload.aspx
@@ -0,0 +1,27 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="FileUpload.aspx.cs" Inherits="WebApplication1.FileUpload" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:SqlDataSource ID="SqlDataSource1" runat="server" ConnectionString="<%$ ConnectionStrings:MyDBConnectionString %>" SelectCommand="SELECT * FROM [file_details]"></asp:SqlDataSource>
+            <asp:FileUpload ID="FileUpload1" runat="server" />
+            <br />
+            <br />
+            <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Upload" />
+            <br />
+            <br />
+            <asp:Label ID="Label1" runat="server" Text="Uploaded files:"></asp:Label>
+            <br />
+            <br />
+            <asp:GridView ID="GridView1" runat="server">
+            </asp:GridView>
+        </div>
+    </form>
+</body>
+</html>
---
+++ WebApplication1/FileUpload.aspx.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.IO;
+using System.Data.SqlClient;
+using System.Data;
+using System.Configuration;
+
+namespace WebApplication1
+{
+    public partial class FileUpload : System.Web.UI.Page
+    {
+        SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["MyDBConnectionString"].ConnectionString);
+        SqlCommand cmd = null;
+        SqlDataReader dr = null;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            if (FileUpload1.HasFile)
+            {
+                string fname = FileUpload1.PostedFile.FileName;
+                string extension = Path.GetExtension(fname);
+                int fsize = FileUpload1.PostedFile.ContentLength;
+                int flag=0;
+                switch(extension.ToLower())
+                {
+                    case ".doc":
+                    case ".docx":
+                    case ".pdf":
+                        flag = 1;
+                        break;
+                    default:
+                        flag = 0;
+                        break;
+                }
+                if(flag==1)
+                {
+                    FileUpload1.SaveAs(Server.MapPath("~/Files/" + fname));
+                    cmd = new SqlCommand("insert into file_details(FileName, FileSize) values('"+fname+"', '"+fsize/1024+"')", con);
+                    con.Open();
+                    if(cmd.ExecuteNonQuery()!=0)
+                    {
+                        Label1.Text = "File Uploaded successfully";
+                        Label1.ForeColor = System.Drawing.Color.Green;
+                        con.Close();
+                        GridDisplayFiles();
+                    }
+                    else
+                    {
+                        Label1.Text = "File failed to upload";
+                    }
+                }
+                else
+                {
+                    Label1.Text = "Only files with extendions .doc, .docx and .pdf files are allowed";
+                }
+            }
+            else
+            {
+                Label1.Text = "Select the file";
+            }
+        }
+
+        private void GridDisplayFiles()
+        {
+            con.Open();
+            cmd = new SqlCommand("SELECT * FROM file_details", con);
+            dr = cmd.ExecuteReader();
+            if (dr.HasRows)
+            {
+                GridView1.DataSource = dr;
+                GridView1.DataBind();
+            }
+            else
+            {
+                Label1.Text = "No files uploaded";
+            }
+        }
+    }
+};
